06c0982e6e3d8e7b23a7a874a3c4f6f8558a0929
Andre Machado <ammachado@gmx.com>
2018-09-10 14:26:33 -0300

    remove unnecessary 'using'

--- nmtLibrary.Data/Repository/WriterRepository.cs
+++ nmtLibrary.Data/Repository/WriterRepository.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using nmtLibrary.Data;
 using nmtLibrary.Domain.Interfaces;
 using nmtLibrary.Domain.Models;
-using nmtLibrary.Repository;
 
 namespace nmtLibrary.Data.Repository
 {
